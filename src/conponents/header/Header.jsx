a313854c624dbbf04283165f9b2f5d129d0073df
IhorC. <igorchua@gmail.com>
2023-11-14 16:45:50 -0600

    header icons added

--- src/conponents/header/Header.jsx
+++ src/conponents/header/Header.jsx
@@ -2,45 +2,47 @@ import { NavLink, Outlet } from "react-router-dom";
 import clsx from "clsx";
 import css from "./Header.module.scss";
 import { Navigation } from "../navigation/Navigation";
+import { useEffect } from "react";
+import sprite from '../../assets/icons/icons.svg';
 export const Header = () => {
-
+    // let screenSize = window.innerWidth > 768 ? true : false;
     const test = () => {
         console.log(window.innerWidth)
         
     }
 
-    // const activeLink = (navData) => {
-    //     // return clsx({
-    //     //     [css.navLink]: true,
-    //     //     [css.active]: navData.isActive ? true : false
-    //     // });
-    //     return clsx(
-    //         css.navLink, 
-    //         navData.isActive && css.active
-    //     )
-    // }
+  
+    
+
+    
+    // useEffect(() => {
+
+    // }, [screenSize]);
 
     return (
         <>
             <header className={css.header}>
                 <div className={css.logoContainer}>
-                    <div className={css.logo}></div>
+                    <div className={css.logo}>
+                        <svg className={css.logoSvg}>
+                            <use href={sprite + '#icon-logo'}/>
+                        </svg>
+                    </div>
                     <p className={css.logoText}>LearnLingo</p>
                 </div>
-                {
-                    window.innerWidth < 768 ? 
-                    <div>
-                        <div className={css.svgBurgerMenu}></div> 
-                        <div>
-                            <Navigation/>
-                        </div>
-                    </div>
-                    : <Navigation/>
-                }
+
+                <div>
+                    <svg className={css.svgBurgerMenu}>
+                        <use href={sprite + '#icon-burger-menu'}/>
+                    </svg>
+                    <Navigation/>
+                </div>
                 
                 <div className={css.authContainer}>
                     <div className={css.auth}>
-                        <div className={css.svgLogIn}></div>
+                        <svg className={css.svgLogIn}>
+                            <use href={sprite + '#icon-log-in'}/>
+                        </svg>
                         <p className={css.loginText}>Log in</p>
                     </div>
                     <button type="button" className={css.registrationBtn}>Registration</button>
