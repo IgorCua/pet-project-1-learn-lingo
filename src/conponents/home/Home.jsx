16c712eb0f8f7ceab34b7b1e7f667c942a5d042b
IhorC. <igorchua@gmail.com>
2023-11-18 21:42:47 -0600

    working with main

--- src/conponents/home/Home.jsx
+++ src/conponents/home/Home.jsx
@@ -1,10 +1,26 @@
 import css from './Home.module.scss';
+import sprite from '../../assets/icons/icons.svg';
 
 export const Home = () => {
     return (
-        <div>
-            <div className={css.getStartedContainer}></div>
-            <div className={css.imageContainer}></div>
+        <div className={css.section}>
+            <div className={css.getStartedContainer}>
+                <h1>Unlock your potential with the best <span>language</span> tutors</h1>
+                <p>
+                    Embark on an Exciting Language Journey with Expert Language Tutors: 
+                    Elevate your language proficiency to new heights by connecting 
+                    with highly qualified and experienced tutors.
+                </p>
+                <button type='button'>Get started</button>
+            </div>
+            <div className={css.imageContainer}>
+                <div className={css.imgDecoration}>
+                    <div className={css.img}/>
+                    <svg className={css.svg}>
+                        <use href={sprite + "#icon-pc-screen"}/>
+                    </svg>
+                </div>
+            </div>
             <div className={css.stat}></div>
         </div>
     )
