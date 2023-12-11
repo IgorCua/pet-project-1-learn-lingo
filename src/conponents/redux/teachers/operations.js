8e762b2113553e28f2f44d88543b0f9489af602b
IhorC. <igorchua@gmail.com>
2023-12-11 16:52:21 -0600

    teachers slice and operations added

--- src/conponents/redux/teachers/operations.js
+++ src/conponents/redux/teachers/operations.js
@@ -1,1 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const getTeachersList = createAsyncThunk(
+    'teachers/getTeachersList',
+
+    async (getTeachersList, {rejectWithValue}) => {
+        try{
+
+        } catch (error) {
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
