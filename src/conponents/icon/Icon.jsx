9ec51e7d096868f512a191373c807cf1c2cc5e12
IhorC. <igorchua@gmail.com>
2024-03-02 14:09:47 -0600

    fix: Icon changed

--- src/conponents/icon/Icon.jsx
+++ src/conponents/icon/Icon.jsx
@@ -1,11 +1,9 @@
 import { clsx } from 'clsx';
 import sprite from '../../assets/icons/icons.svg';
-import css from './Icon.module.scss';
  
 const Icon = ({name, className, secondaryClassName = null}) => {
     return <svg
         className={clsx(
-            css.svg,
             className && className,
             secondaryClassName && secondaryClassName
         )}
